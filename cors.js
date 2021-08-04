f7896e46d37b8bf9100c8261f8a92d6bf6bae542
lizuncong <lizuncong@joyy.sg>
2021-08-04 19:28:32 +0800

    stash

--- cors.js
+++ cors.js
@@ -65,25 +65,37 @@
 //       next()
 //    }
 // });
-const express = require('express');
-
+const express = require('express')
+const querystring = require('querystring')
 const app = express();
 app.use((req, res, next) => {
    if(req.method === 'OPTIONS'){
       // 预检请求
       res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Headers', 'X-PINGOTHER');
       res.end();
    } else {
       // 普通请求
-      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9001');
+      res.setHeader('Vary', 'Origin');
+      res.setHeader('Access-Control-Allow-Credentials', 'true');
       next()
    }
 });
-app.get('/get', (request, response) => {
-    response.setHeader('X-My-Custom-Header', 'test custom header');
-    response.setHeader('Access-Control-Expose-Headers', 'X-My-Custom-Header');
-    response.json({method: 'get'})
+app.get('/login', (req, res) => {
+   res.setHeader('Set-Cookie', ['type=ninja;Secure;SameSite=None;', 'language=javascript;Secure', 'name=lzc;SameSite=Strict']);
+   res.json({method: 'get', cookie: req.headers.cookie})
+});
+app.get('/get', (req, res) => {
+   const urlSplitData = req.url.split('?')
+   req.path = urlSplitData[0]
+   req.query = querystring.parse(urlSplitData[1])
+   const time = (req.query.id * 1000) + Math.round(Math.random() * 10) * 100
+   console.log('get..', req.query.id)
+   res.setHeader('Connection', 'Keep-Alive');
+   res.setHeader('timeout', '60');
+   setTimeout(() => {
+      res.json({method: 'get', id: req.query.id, time: `${time}ms` })
+   }, time)
 });
 app.listen(3000, () => {
   console.log(`service listening at http://localhost:3000`)
